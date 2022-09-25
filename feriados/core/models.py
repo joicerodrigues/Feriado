e3928532c9703cf01c5fbe0bd2a6e16d6d3346d1
Joice <joicerodrigues020@gmail.com>
2022-09-24 23:09:09 -0300

    V04

--- feriados/core/models.py
+++ feriados/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class FeriadoModel(models.Model)
+    nome = models.CharField('Feriado',max_length=50)
+    dia = models.IntegerField('Data')
+    mes = models.IntegerField('Mês')
+    modificado_em = models.DateTimeField(
+        verbose_name='modificado em',
+        auto_now_add=False, auto_now=True)
+
+    def _str_(self):
+        return self.nome
